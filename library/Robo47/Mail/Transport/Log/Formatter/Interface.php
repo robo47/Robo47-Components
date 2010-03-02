40d7cc381d4d3889a23326b21cdc833cc19531b0
Benjamin Steininger (robo47) <robo47@robo47.net>
2010-03-02 16:05:28 +0100

    added PHPDOC-Block to Robo47_Mail_Transport_Log_Formatter_Interface && some changes to README

--- library/Robo47/Mail/Transport/Log/Formatter/Interface.php
+++ library/Robo47/Mail/Transport/Log/Formatter/Interface.php
@@ -1,5 +1,33 @@
 <?php
 
+/**
+ * Robo47 Components
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://robo47.net/licenses/new-bsd-license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to robo47[at]robo47[dot]net so I can send you a copy immediately.
+ *
+ * @category   Robo47
+ * @package    Robo47
+ * @copyright  Copyright (c) 2007-2010 Benjamin Steininger (http://robo47.net)
+ * @license    http://robo47.net/licenses/new-bsd-license New BSD License
+ */
+/**
+ * Robo47_Mail_Transport_Log_Formatter_Interface
+ *
+ * @package     Robo47
+ * @subpackage  Mail
+ * @since       0.1
+ * @copyright   Copyright (c) 2007-2010 Benjamin Steininger (http://robo47.net)
+ * @license     http://robo47.net/licenses/new-bsd-license New BSD License
+ * @author      Benjamin Steininger <robo47[at]robo47[dot]net>
+ */
 interface Robo47_Mail_Transport_Log_Formatter_Interface
 {
     /**
