c3577b3be23afce0a2ee2fb260a00cfb1558a40d
Benjamin Steininger <robo47@robo47.net>
2012-02-04 20:00:26 +0100

    script

--- bin/ci/travis-ci.sh
+++ bin/ci/travis-ci.sh
@@ -1,7 +1,7 @@
 mkdir -p vendor
 wget -dv http://ezcomponents.org/files/downloads/ezcomponents-2009.2.1-lite.tar.bz2
-tar -xzvf ezcomponents-2009.2.1-lite.tar.bz2 -c vendor
-mv vendor/ezcomponents-2009.2.1-lite vendor/ezc
+tar -xjf ezcomponents-2009.2.1-lite.tar.bz2 -C vendor
+mv vendor/ezcomponents-2009.2.1 vendor/ezc
 rm ezcomponents-2009.2.1-lite.tar.bz2
 
 git clone git://github.com/ezyang/htmlpurifier.git vendor/htmlpurifier
